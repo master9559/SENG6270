650f663c84bd8c46deb4e16884665e9475f1e055
Michael Claar <michael@claar.net>
2018-02-17 16:54:30 -0500

    Adding comment for testing

--- 6270PersonalAttempt/Order.cs
+++ 6270PersonalAttempt/Order.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace _6270PersonalAttempt
 {
+    /// <summary>
+    /// Processing totals here..
+    /// </summary>
     class Order
     {
         public List<PhotoLineItem> lines;
